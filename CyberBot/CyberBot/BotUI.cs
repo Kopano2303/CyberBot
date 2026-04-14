f1abaa143a6a9918c7856d4acbd381dbc86e358f
Kopano2303 <ST10491894@rcconnect.edu.za>
2026-04-14 15:32:28 +0200

    commit

--- CyberBot/CyberBot/BotUI.cs
+++ CyberBot/CyberBot/BotUI.cs
@@ -12,8 +12,6 @@ namespace CyberBot
     {
         public static void Run()
         {
-
-            ConsoleUI.SetTitle("CyberBot - Security Assistant");
             PlayGreeting();
             
             ConsoleUI.DrawHeader("CyberSecurity Awareness");
